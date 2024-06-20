759b1d9d0299943b784a610257637e2b5c7303b2
Stephen Allen <Staylorallen@Gmail.com>
2024-06-20 17:12:31 -0400

    More consistant test setup

--- server/api/pet/pet.test.js
+++ server/api/pet/pet.test.js
@@ -20,31 +20,57 @@ import Pet from "./petModel.js";
 // ---- test valid data
 
 describe("server/api/pet", () => {
-  let cookie;
-  let refUser_id;
+  // variables - declare
+  // user 1 is for general tests
+  let user1 = {
+    cookie: "",
+    _id: "",
+    userObject: {
+      username: "petTestUser1",
+      email: "petTestUser1@t.t",
+      password: "petTestUser1",
+    },
+  };
+  // user 2 is for testing pet ownership (cannot access if not owner)
+  let user2 = {
+    cookie: "",
+    _id: "",
+    userObject: {
+      username: "petTestUser2",
+      email: "petTestUser2@t.t",
+      password: "petTestUser2",
+    },
+  };
 
-  // beforeAll
+  // beforeAll (create anything needed for testing pet endpoints)
   beforeAll(async () => {
     const mongoDBUrl = process.env.MONGODB_URI_TESTING;
     await mongoose.connect(mongoDBUrl);
 
-    // create a user
-    const userObj = {
-      username: "petTestUser",
-      email: "petTestUser@t.t",
-      password: "petTestUser",
-    };
-    const newUser = new User(userObj);
+    // user1: create
+    const newUser = new User(user1.userObject);
     await newUser.save();
-    refUser_id = newUser._id;
+    user1._id = newUser._id;
 
-    // login to get session cookie
+    // user2: create
+    const newUser2 = new User(user2.userObject);
+    await newUser2.save();
+    user2._id = newUser2._id;
+
+    // user1: login and get cookie
     const res = await request(app)
       .post("/api/user/login")
-      .send({ email: userObj.email, password: userObj.password });
-    cookie = res.headers["set-cookie"];
+      .send({ email: user1.userObject.email, password: user1.userObject.password });
+    user1.cookie = res.headers["set-cookie"];
+
+    // user2: login and get cookie
+    const res2 = await request(app)
+      .post("/api/user/login")
+      .send({ email: user2.userObject.email, password: user2.userObject.password });
+    user2.cookie = res2.headers["set-cookie"];
   });
-  // afterAll
+
+  // afterAll (drop anything created for testing pet endpoints)
   afterAll(async () => {
     await mongoose.connection.db.dropDatabase();
     await mongoose.connection.close();
@@ -58,26 +84,30 @@ describe("server/api/pet", () => {
         expect(res.statusCode).toBe(401);
       });
       test("no name: should return 400", async () => {
-        const res = await request(app).post("/api/pet/").set("Cookie", cookie).send({});
+        const res = await request(app)
+          .post("/api/pet/")
+          .set("Cookie", user1.cookie)
+          .send({});
         expect(res.statusCode).toBe(400);
       });
       test("name empty string: should return 400", async () => {
         const res = await request(app)
           .post("/api/pet/")
-          .set("Cookie", cookie)
+          .set("Cookie", user1.cookie)
           .send({ name: "" });
         expect(res.statusCode).toBe(400);
       });
     }); // end invalid data
     describe("valid data", () => {
       let petCreateObject = { name: "petTest" };
-      test("should return 200 and pet object", async () => {
+      test("should return 200 and cleaned pet object", async () => {
         const res = await request(app)
           .post("/api/pet/")
-          .set("Cookie", cookie)
+          .set("Cookie", user1.cookie)
           .send(petCreateObject);
         expect(res.statusCode).toBe(200);
         expect(res.body.pet.name).toBe(petCreateObject.name);
+        expect(res.body.pet.refUser_id).toBeUndefined();
       });
     }); // end valid data
   }); // end POST /api/pet/
@@ -92,7 +122,7 @@ describe("server/api/pet", () => {
     }); // end invalid data
     describe("valid data", () => {
       test("should return 200 and array of pets", async () => {
-        const res = await request(app).get("/api/pet/").set("Cookie", cookie);
+        const res = await request(app).get("/api/pet/").set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(200);
         expect(res.body.pets).toBeInstanceOf(Array);
       });
@@ -107,25 +137,31 @@ describe("server/api/pet", () => {
         expect(res.statusCode).toBe(401);
       });
       test("invalid pet_id: should return 400", async () => {
-        const res = await request(app).get("/api/pet/1").set("Cookie", cookie);
+        const res = await request(app).get("/api/pet/1").set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(400);
       });
     }); // end invalid data
     describe("valid data", () => {
-      let pet_id;
-      let petGetOneObject;
-
+      let petCreate1;
       beforeAll(async () => {
-        petGetOneObject = { refUser_id, name: "petGetOneTest1" };
+        petCreate1 = {
+          pet_id: null,
+          petObject: {
+            refUser_id: user1._id,
+            name: "petGetOneTest1",
+          },
+        };
 
-        const pet = new Pet(petGetOneObject);
+        const pet = new Pet(petCreate1.petObject);
         await pet.save();
-        pet_id = pet._id;
+        petCreate1.pet_id = pet._id;
       });
       test("should return 200 and pet object", async () => {
-        const res = await request(app).get(`/api/pet/${pet_id}`).set("Cookie", cookie);
+        const res = await request(app)
+          .get(`/api/pet/${petCreate1.pet_id}`)
+          .set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(200);
-        expect(res.body.pet.name).toBe(petGetOneObject.name);
+        expect(res.body.pet.name).toBe(petCreate1.petObject.name);
       });
     }); // end valid data
   }); // end GET /api/pet/:pet_id
@@ -138,25 +174,29 @@ describe("server/api/pet", () => {
         expect(res.statusCode).toBe(401);
       });
       test("invalid pet_id: should return 400", async () => {
-        const res = await request(app).patch("/api/pet/1").set("Cookie", cookie);
+        const res = await request(app).patch("/api/pet/1").set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(400);
       });
     }); // end invalid data
     describe("valid data", () => {
-      let pet_id;
-      let petUpdateObject;
-
+      let petUpdate1;
       beforeAll(async () => {
-        petUpdateObject = { refUser_id, name: "petUpdateTest1" };
+        petUpdate1 = {
+          pet_id: null,
+          petObject: {
+            refUser_id: user1._id,
+            name: "petUpdateTest1",
+          },
+        };
 
-        const pet = new Pet(petUpdateObject);
+        const pet = new Pet(petUpdate1.petObject);
         await pet.save();
-        pet_id = pet._id;
+        petUpdate1.pet_id = pet._id;
       });
       test("should return 200 and pet object", async () => {
         const res = await request(app)
-          .patch(`/api/pet/${pet_id}`)
-          .set("Cookie", cookie)
+          .patch(`/api/pet/${petUpdate1.pet_id}`)
+          .set("Cookie", user1.cookie)
           .send({ name: "petTestUpdate" });
         expect(res.statusCode).toBe(200);
         expect(res.body.pet.name).toBe("petTestUpdate");
@@ -172,25 +212,32 @@ describe("server/api/pet", () => {
         expect(res.statusCode).toBe(401);
       });
       test("invalid pet_id: should return 400", async () => {
-        const res = await request(app).delete("/api/pet/1").set("Cookie", cookie);
+        const res = await request(app).delete("/api/pet/1").set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(400);
       });
     }); // end invalid data
     describe("valid data", () => {
-      let pet_id;
-      let petDeleteObject;
+      let petDelete1;
 
       beforeAll(async () => {
-        petDeleteObject = { refUser_id, name: "petDeleteTest1" };
+        petDelete1 = {
+          pet_id: null,
+          petObject: {
+            refUser_id: user1._id,
+            name: "petDeleteTest1",
+          },
+        };
 
-        const pet = new Pet(petDeleteObject);
+        const pet = new Pet(petDelete1.petObject);
         await pet.save();
-        pet_id = pet._id;
+        petDelete1.pet_id = pet._id;
       });
       test("should return 200 and pet object", async () => {
-        const res = await request(app).delete(`/api/pet/${pet_id}`).set("Cookie", cookie);
+        const res = await request(app)
+          .delete(`/api/pet/${petDelete1.pet_id}`)
+          .set("Cookie", user1.cookie);
         expect(res.statusCode).toBe(200);
-        expect(res.body.pet.name).toBe(petDeleteObject.name);
+        expect(res.body.pet.name).toBe(petDelete1.petObject.name);
       });
     }); // end valid data
   }); // end DELETE /api/pet/:pet_id
